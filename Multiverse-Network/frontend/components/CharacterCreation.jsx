2765fba324d8f0188c85bfe74a1745c78ba7f156
Danyelle Rogers <danyelleish@gmail.com>
2026-03-02 19:23:41 -0800

    Add files via upload
    Signed-off-by: Danyelle Rogers <danyelleish@gmail.com>

---
+++ Multiverse-Network/frontend/components/CharacterCreation.jsx
@@ -0,0 +1,12 @@
+// CharacterCreation Component
+import React from 'react';
+
+const CharacterCreation = () => {
+    return (
+        <div>
+            <h1>Character Creation</h1>
+        </div>
+    );
+};
+
+export default CharacterCreation;
